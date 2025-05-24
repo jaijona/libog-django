b3761e514852f3c1af6fc2ec32d417e3e739a856
Jonathan <jaijona@gmail.com>
2025-05-24 15:46:45 -0500

    actu tabla

--- sistema_login/settings.py
+++ sistema_login/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Carpeta con tus archivos estáticos
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta donde Django los copiará para producción
