b46d514927941862dd39270c4f8b1722923bc29f
Jonathan <jaijona@gmail.com>
2025-05-24 18:24:07 -0500

    actu si posicion

--- posicion/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PosicionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posicion'
--- posicion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('mostrar_datos/', views.mostrar_datos, name='mostrar_datos'),
-    path('actualizar/', views.actualizar_tabla, name='actualizar_tabla'),
-]
