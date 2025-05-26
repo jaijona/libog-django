2e9035e4ff6bed5849a4e2b1351c1b885dc75e9a
Jonathan <jaijona@gmail.com>
2025-05-26 18:46:51 -0500

    cambio de static a global

--- sistema_login/settings.py
+++ sistema_login/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static'),  # Carpeta con tus archivos estáticos
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),  # Carpeta con tus archivos estáticos
+]
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta donde Django los copiará para producción
